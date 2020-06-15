db6d13f293e56c1225eb0fb065d2092dc27eec0b
RadoslawGorniak <radekg08@wp.pl>
2020-06-15 22:25:09 +0200

    Initial commit

---
+++ src/test/java/com/example/formypet/FormypetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormypetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
